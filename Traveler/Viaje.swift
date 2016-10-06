b711dc6e41091b5860dbdb67f6d4a0efb7e6ec03
Felipe Holguin <luis.felipe.holguin@hotmail.com>
2016-10-06 12:11:45 -0500

    first commit

---
+++ Traveler/Viaje.swift
@@ -0,0 +1,9 @@
+//
+//  Viaje.swift
+//  Traveler
+//
+//  Created by Luis Felipe Holguin on 21/06/16.
+//  Copyright © 2016 Luis Felipe Holguin. All rights reserved.
+//
+
+import Foundation
---
+++ Traveler/ViajeCell.swift
@@ -0,0 +1,13 @@
+//
+//  ViajeCell.swift
+//  Traveler
+//
+//  Created by Luis Felipe Holguin on 21/06/16.
+//  Copyright © 2016 Luis Felipe Holguin. All rights reserved.
+//
+
+import Cocoa
+
+class ViajeCell: UITableViewCell {
+
+}
